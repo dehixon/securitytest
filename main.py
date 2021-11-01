faec591b731e00ae1e268dd487007cd467a14362
dehixon <dehixon@my.waketech.edu>
2021-11-01 11:45:54 -0400

    First unit test of writing hashed passwords to file

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from passGen import passGen
 
-test = passGen()
+test = passGen(0, None)
 valid = test.verify()
 while not valid:
     print('Password was incorrect.')
     valid = test.verify()
 
 print('Password verified')
+file = open("passwords.txt", "wb")
+file.write(test.store + b'\n')
+file.write(test.store + b'\n')
+file.close()
+print("File written")
